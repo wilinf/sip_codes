7a12357c72eed8668ac1e031872e5ea2830a7d23
wilinf <sixiang.zhao@hotmail.com>
2020-05-29 00:16:04 +0800

    liu 2 is added

--- test_problems/SIP_csa_algorithm_liu2.py
+++ test_problems/SIP_csa_algorithm_liu2.py
@@ -1,4 +1,4 @@
-# SIP practice: problem liu1
+# SIP practice: problem liu2
 import numpy as np
 import math
 import numpy.matlib
@@ -25,17 +25,17 @@ class CSA:
         # self.grad_objective = np.array([0, 1])  # gradients of the obj function  2*x1 -4, 2*x2 - 2
 
         # ========= decision variables ========= #
-        self.x_ub = 5  # np.inf
-        self.x_lb = - 5  # -np.inf
+        self.x_ub = 10  # np.inf
+        self.x_lb = - 10  # -np.inf
 
         # ========= Delta region ========= #
         self.delta_lb = 0
-        self.delta_ub = 2
+        self.delta_ub = np.pi / 2
 
         # ========= calculate the parameters ========= #
-        self.l_g_x = 5  # given any delta, L-constant of g
-        self.l_f = 14  # sup|gradient(f)|
-        self.l_g_delta = max(4 * self.x_ub,  1)  # ## given any x, L-constant of g(x, delta)
+        self.l_g_x = 1  # given any delta, L-constant of g
+        self.l_f = 6 * self.x_ub + 4  # sup|gradient(f)|
+        self.l_g_delta = 1  # ## given any x, L-constant of g(x, delta)
         self.d_x = (self.x_ub - self.x_lb) * np.sqrt(self.x_dim)  # diameter of the domain x
         self.d_delta = self.delta_ub - self.delta_lb  # diameter of delta
         self.r_delta = (self.delta_ub - self.delta_lb) / 2  # radius of the largest ball which can be included in Delta
@@ -99,27 +99,27 @@ class CSA:
         return arr_g_grad_max, temp0
 
     def calculate_g_grad_and_values_arr(self, x, theta_rand_value):
-        g_values = - x[0] + (1 + theta_rand_value ** 2) * x[1]  # values in abs
-        g_grad = np.array([- np.ones(len(theta_rand_value)), (1 + theta_rand_value ** 2)])
+        g_values = np.cos(theta_rand_value) * x[0] + np.sin(theta_rand_value) * x[1] - 1  # values in abs
+        g_grad = np.array([- np.sin(theta_rand_value), np.cos(theta_rand_value)])
         return g_grad, g_values
 
     def calculate_g_grad_and_values(self, x, theta_rand_value):
-        g_values = - x[0] + (1 + theta_rand_value ** 2) * x[1]  # values in abs
-        g_grad = np.array([- 1, (1 + theta_rand_value ** 2)])
+        g_values = np.cos(theta_rand_value) * x[0] + np.sin(theta_rand_value) * x[1] - 1  # values in abs
+        g_grad = np.array([- np.sin(theta_rand_value), np.cos(theta_rand_value)])
         return g_grad, g_values
 
     def calculate_f_grad(self, x_sol):
-        if len(x_sol.shape) == 1:
-            f_grad = 2 * x_sol - np.array([4, 2])
-        else:
-            f_grad = 2 * x_sol - np.tile(np.array([[4], [2]]), [1, x_sol.shape[1]])
+        f_grad = np.array([4 * x_sol[0] - 2 * x_sol[1] - 2,
+                           4 * x_sol[1] - 2 * x_sol[0] - 4])
         return f_grad
 
     def calculate_f_values(self, x_sol):
         if len(x_sol.shape) == 1:
-            f_values = x_sol[0] ** 2 + x_sol[1] ** 2 - 4 * x_sol[0] - 2 * x_sol[1]
+            f_values = 2 * x_sol[0] ** 2 + 2 * x_sol[1] ** 2 \
+                       - 2 * x_sol[0] * x_sol[1] - 2 * x_sol[0] - 4 * x_sol[1]
         else:
-            f_values = x_sol[0, :] ** 2 + x_sol[1, :] ** 2 - 4 * x_sol[0, :] - 2 * x_sol[1, :]
+            f_values = 2 * x_sol[0, :] ** 2 + 2 * x_sol[1, :] ** 2 \
+                       - 2 * x_sol[0, :] * x_sol[1, :] - 2 * x_sol[0, :] - 4 * x_sol[1, :]
         return f_values
 
     def csa_algorithm(self):
@@ -159,6 +159,8 @@ class CSA:
 
             # if self.plot_setB is True:
             self.accumulate_num_of_bb[k] = len(index_set)
+            if k % 1000 == 0:
+                aa = 1
 
         x_sum = np.zeros(self.x_dim)
         gamma_sum = 0
@@ -243,17 +245,17 @@ def main():
     np.random.seed(1)
     parse_input = {
         'epsilon': 0.01,
-        'num_iterations': 1000,
-        'c_gamma': 0.5,  # 0.12
-        'c_eta': 0.0005,  # 0.0005
-        'x0': np.array([0, 0])  # according to the paper, the third one is eta, whose maximum is 5.389 if x=(1,1)
+        'num_iterations': 10000,
+        'c_gamma': 0.3,  # 0.03
+        'c_eta': 0.001,  # 0.0005
+        'x0': np.array([1, 1])  # according to the paper, the third one is eta, whose maximum is 5.389 if x=(1,1)
     }
     csa = CSA(parse_input)
 
     # plt.rc('text', usetex=True)
     # plt.rc('font', family='serif')
 
-    csa.run_adaptive_sampling(10, 20)
+    csa.run_adaptive_sampling(10, 50)
     # csa.run_fixed_sampling(1000)
     csa.plot_x_last_iterate()
     csa.plot_x_bar()
